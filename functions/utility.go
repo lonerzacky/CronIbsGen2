388ea29dc1f35754b2773ec3efddc3b2775ff2f7
wolfz <azhar.zakiy@gmail.com>
2019-05-23 12:36:44 +0700

    logger functions

--- functions/utility.go
+++ functions/utility.go
@@ -3,6 +3,7 @@ package functions
 import (
 	"database/sql"
 	"fmt"
+	"github.com/op/go-logging"
 	"github.com/vjeantet/jodaTime"
 	"net"
 	"os"
@@ -44,4 +45,18 @@ func ParseTimeScheduler(Time string) string {
 		return ""
 	}
 	return t.Format("15:04")
+}
+
+func Logger() *logging.Logger {
+	var log = logging.MustGetLogger("main")
+	var format = logging.MustStringFormatter(
+		`%{color}%{time:15:04:05.000} %{shortfunc} ► %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+	)
+	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend2 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend2Formatter := logging.NewBackendFormatter(backend2, format)
+	backend1Leveled := logging.AddModuleLevel(backend1)
+	backend1Leveled.SetLevel(logging.ERROR, "")
+	logging.SetBackend(backend1Leveled, backend2Formatter)
+	return log
 }
